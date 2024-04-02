5deaad24e00eff01c8de52529223df94c2042835
et120 <e.trotter120@gmail.com>
2024-04-02 11:59:45 -0700

    radio buttons add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+'use client'
+
+import { useState } from "react";
 import IconDollarComponent from "./Components/IconDollarComponent";
 import IconPersonComponent from "./Components/IconPersonComponent";
 import LogoComponent from "./Components/LogoComponent";
 
 export default function Home() {
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const handleRadioChange = (value: string) => {
+    setSelectedValue(value);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24 bg-lightGrayCyan">
       <div>
@@ -15,37 +24,98 @@ export default function Home() {
           <div className="bg-lightGray h-10 rounded flex justify-between items-center px-4">
             <IconDollarComponent />
             <div className="flex-grow">
-              <input type="text" placeholder="0" className="w-full text-right outline-none bg-transparent" />
+              <input
+                type="text"
+                placeholder="0"
+                className="w-full text-right outline-none bg-transparent"
+              />
             </div>
           </div>
 
           <p className="pt-10 pb-3">Select Tip %</p>
-          <div className="grid grid-cols-3 gap-3">
-            <button className="bg-darkCyan rounded flex justify-center items-center h-10 w-20 text-white">
-              <p>5%</p>
-            </button>
-            <button className="bg-darkCyan rounded flex justify-center items-center h-10 w-20 text-white">
-              <p>10%</p>
-            </button>
-            <button className="bg-darkCyan rounded flex justify-center items-center h-10 w-20 text-white">
-              <p>15%</p>
-            </button>
-            <button className="bg-darkCyan rounded flex justify-center items-center h-10 w-20 text-white">
-              <p>25%</p>
-            </button>
-            <button className="bg-darkCyan rounded flex justify-center items-center h-10 w-20 text-white">
-              <p>50%</p>
-            </button>
-            <button className="bg-lightGray rounded flex justify-center items-center h-10 w-20 text-darkGrayCyan">
-              <p>Custom</p>
-            </button>
+          <div className="grid grid-cols-3 gap-3 text-lg">
+            <label
+              className={`${selectedValue === '5%' ? 'bg-strongCyan' : 'bg-darkCyan'} rounded flex justify-center items-center h-10 text-white hover:bg-lightGrayCyan hover:text-darkCyan cursor-pointer`}
+              style={{ width: 90 }}
+            >
+              <input
+                type="radio"
+                name="percentage"
+                value="5%"
+                className="appearance-none"
+                onChange={() => handleRadioChange('5%')} checked={selectedValue === '5%'}
+              />
+              <span>5%</span>
+            </label>
+            <label
+              className={`${selectedValue === '10%' ? 'bg-strongCyan' : 'bg-darkCyan'} rounded flex justify-center items-center h-10 text-white hover:bg-lightGrayCyan hover:text-darkCyan cursor-pointer`}
+              style={{ width: 90 }}
+            >
+              <input
+                type="radio"
+                name="percentage"
+                value="10%"
+                className="appearance-none"
+                onChange={() => handleRadioChange('10%')} checked={selectedValue === '10%'}
+              />
+              <span>10%</span>
+            </label>
+            <label
+              className={`${selectedValue === '15%' ? 'bg-strongCyan' : 'bg-darkCyan'} rounded flex justify-center items-center h-10 text-white hover:bg-lightGrayCyan hover:text-darkCyan cursor-pointer`}
+              style={{ width: 90 }}
+            >
+              <input
+                type="radio"
+                name="percentage"
+                value="15%"
+                className="appearance-none"
+                onChange={() => handleRadioChange('15%')} checked={selectedValue === '15%'}
+              />
+              <span>15%</span>
+            </label>
+            <label
+              className={`${selectedValue === '25%' ? 'bg-strongCyan' : 'bg-darkCyan'} rounded flex justify-center items-center h-10 text-white hover:bg-lightGrayCyan hover:text-darkCyan cursor-pointer`}
+              style={{ width: 90 }}
+            >
+              <input
+                type="radio"
+                name="percentage"
+                value="25%"
+                className="appearance-none"
+                onChange={() => handleRadioChange('25%')} checked={selectedValue === '25%'}
+              />
+              <span>25%</span>
+            </label>
+            <label
+              className={`${selectedValue === '50%' ? 'bg-strongCyan' : 'bg-darkCyan'} rounded flex justify-center items-center h-10 text-white hover:bg-lightGrayCyan hover:text-darkCyan cursor-pointer`}
+              style={{ width: 90 }}
+            >
+              <input
+                type="radio"
+                name="percentage"
+                value="50%"
+                className="appearance-none"
+                onChange={() => handleRadioChange('50%')} checked={selectedValue === '50%'}
+              />
+              <span>50%</span>
+            </label>
+            <input
+              type="text"
+              className="bg-lightGray rounded text-center h-10 text-darkGrayCyan"
+              placeholder="Custom"
+              style={{ width: 90 }}
+            />
           </div>
 
           <p className="pt-10 pb-1">Number of People</p>
           <div className="bg-lightGray h-10 rounded flex justify-between items-center px-4">
             <IconPersonComponent />
             <div className="flex-grow">
-              <input type="text" placeholder="0" className="w-full text-right outline-none bg-transparent" />
+              <input
+                type="text"
+                placeholder="0"
+                className="w-full text-right outline-none bg-transparent"
+              />
             </div>
           </div>
         </div>
@@ -73,7 +143,7 @@ export default function Home() {
             </div>
           </div>
 
-          <button className="bg-strongCyan flex justify-center items-center h-8 rounded">
+          <button className="bg-strongCyan hover:bg-lightGrayCyan hover:text-darkCyan flex justify-center items-center h-8 rounded">
             <p className="text-darkCyan">RESET</p>
           </button>
         </div>
